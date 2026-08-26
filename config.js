a3a040cba4191a523973e7f157e9070f35e41943
Egor-Ryzhikov <egorryzhikov67@gmail.com>
2026-08-26 17:43:05 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+/*
+    FIREBASE config / database
+    to edit !
+*/
+const firebaseConfig = {
+    apiKey: null,
+    authDomain: null,
+    databaseURL: null,
+    projectId: null,
+    storageBucket: null,
+    messagingSenderId: null,
+    appId: null,
+    measurementId: null
+};
