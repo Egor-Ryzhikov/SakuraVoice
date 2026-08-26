28a192194b6401d5484c175083ad859c4ed42d87
Egor-Ryzhikov <egorryzhikov67@gmail.com>
2026-08-27 02:56:41 +0500

    Delete web directory

--- web/src/util/queryParam.ts
+++
@@ -1,5 +0,0 @@
-export const objectToQueryParams = (queryParams: any) =>
-  Object.keys(queryParams)
-    .filter(key => queryParams[key] !== null)
-    .map(key => `${key}=${queryParams[key]}`)
-    .join("&")
