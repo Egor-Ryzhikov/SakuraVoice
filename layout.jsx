4c41bfe84156c82f6529d4d6099c93ff27b3b4e6
Egor-Ryzhikov <egorryzhikov67@gmail.com>
2026-08-27 20:39:01 +0500

    Delete layout.jsx

--- layout.jsx
+++
@@ -1,3 +0,0 @@
-import "./globals.css";
-export const metadata={title:"Yummy Anime — Аниме с озвучкой",description:"Аниме каталог и просмотр"};
-export default function Layout({children}){return <html lang="ru"><body>{children}</body></html>}
